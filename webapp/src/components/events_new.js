72e5984edd7c61a60af0177a6ab8aaa83712166a
小林和希 <kobayashi.kazuki74@gmail.com>
2018-10-21 16:01:08 +0900

    imported necessary package from redux-form

--- webapp/src/components/events_new.js
+++ webapp/src/components/events_new.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
+import { reduxForm, Field } from 'redux-form';
 
 // import { postEvents } from '../actions'
 
