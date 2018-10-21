32b5effd8672804f10a8612f245876374d3c26b6
小林和希 <kobayashi.kazuki74@gmail.com>
2018-10-21 15:32:46 +0900

    implemented events new component

---
+++ webapp/src/components/events_new.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+
+// import { postEvents } from '../actions'
+
+class EventsNew extends Component {
+  render() {
+
+    return (
+      <React.Fragment>
+        <p>Hi!</p>
+      </React.Fragment>
+    )
+  }
+}
+
+// const mapDispatchToProps = dispatch => ({
+//   increment: () => dispatch(increment()),
+//   decrement: () => dispatch(decrement())
+// })
+
+// const mapDispatchToProps = ({ postEvents })
+
+export default connect(null, null)(EventsNew)
