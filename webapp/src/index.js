29090cab9f786f250ab02465d8cb55bf119a9dd2
小林和希 <kobayashi.kazuki74@gmail.com>
2018-10-21 15:20:49 +0900

    implemented router to events_new

--- webapp/src/index.js
+++ webapp/src/index.js
@@ -1,18 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore, applyMiddleware } from 'redux'
-import { Provider } from 'react-redux'
-import thunk from 'redux-thunk'
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
 
 import './index.css';
 import reducer from './reducers'
 import EventsIndex from './components/events_index';
+import EventsNew from './components/events_new';
 import registerServiceWorker from './registerServiceWorker';
 
 const store = createStore(reducer, applyMiddleware(thunk))
 
 ReactDOM.render(
   <Provider store={store}>
+    <BrowserRouter>
+    	<Switch>
+    		<Route exact path="/" components={EventsIndex} />
+    		<Route exact path="/events/new" components={EventsNew} />
+    	</Switch>
+    </BrowserRouter>
     <EventsIndex />
   </Provider>,
   document.getElementById('root')
